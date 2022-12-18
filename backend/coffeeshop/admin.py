13d3dfc18be4c7bdf5c18d2cd38a0ff71fe94e78
Leonardo Araujo <leoujooficial@gmail.com>
2022-12-18 20:40:33 -0300

    relation between coffeshops, categories and items

--- backend/coffeeshop/admin.py
+++ backend/coffeeshop/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Coffeeshop, Category
+from .models import Coffeeshop, Category, Item
 
-# Register your models here.
+
+class Items(admin.ModelAdmin):
+    list_display = ('name', 'price')
+   #  list_display_links = ('name', 'price') -> Both clickable
+   # Allow to search the items by name and price.
+    search_fields = ('name', 'price')
+   #  Create a automatic filter on the items, by name and price.
+    list_filter = ('name', 'price')
+   #  Create pages with the maximum of 10 items each.
+    list_per_page = 10
+
+
 admin.site.register(Coffeeshop)
 admin.site.register(Category)
+admin.site.register(Item, Items)
--- backend/coffeeshop/models.py
+++ backend/coffeeshop/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 
-# In Django, one to many is ForeignKey
+
+class Item(models.Model):
+    photo_url = models.URLField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=3, decimal_places=0, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    items = models.ManyToManyField(Item)
+
+    class Meta:
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
 
 class Coffeeshop(models.Model):
-    logo_url = models.URLField(max_length=200)
+    logo_url = models.URLField(max_length=200, null=True, blank=True)
     name = models.CharField(max_length=200)
     categories = models.ManyToManyField(Category)
 
