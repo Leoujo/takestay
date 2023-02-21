8de8b9d1838cf4ce5903f4ee38ec15b56e7a9868
Leonardo Araujo <leoujooficial@gmail.com>
2023-02-21 20:06:01 -0300

    alerts solved

--- frontend/src/common/components/Snackbar/index.tsx
+++ frontend/src/common/components/Snackbar/index.tsx
@@ -9,16 +9,13 @@ interface SnackbarsProps {
 export const Snackbars: React.FC<SnackbarsProps> = ({ isError }) => {
   const [open, setOpen] = React.useState(false);
 
-  React.useEffect(() => {
-    if (isError) {
-      handleOpen();
-    }
-  }, [isError]);
-
   const handleOpen = () => {
     setOpen(!open);
   };
 
+  if (isError) {
+    handleOpen();
+  }
   return (
     <Snackbar anchorOrigin={{ vertical: "top", horizontal: "center" }} open={open} onClose={handleOpen} message="I love snacks">
       <Alert severity="error">Error! Please, try again.</Alert>
