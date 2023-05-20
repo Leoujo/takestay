a139de95389b4980fc2f3fbc408a10e4c2c7655b
Leonardo Araujo <leoujooficial@gmail.com>
2023-05-20 12:25:09 -0300

    basic frontend token validation

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,52 +1,19 @@
-import { useQuery } from "react-query";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import { CoffeeShop } from "../common/models";
 import { Login } from "../pages/login";
 import { Menu } from "../pages/menu";
 import { QrCode } from "../pages/qrcode";
-import { getAllCoffeeShops, getCoffeeShop } from "../api/services/coffeeshops";
-import { removeSpacesAndAccents } from "../utils";
-import { useSelector } from "react-redux";
-import { RootState } from "../store/store";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 export const Router = () => {
-  const { user } = useSelector((state: RootState) => state);
-
-  // Get all coffee shops
-  const {
-    data: coffeeShops,
-    refetch: coffeeShopsRefetch,
-    isLoading: coffeeShopsIsFetching,
-  } = useQuery(["allCoffeeShops"], (): Promise<CoffeeShop[]> => getAllCoffeeShops(), {
-    retry: false,
-  });
-
-  // Get coffee shop by a owner
-  const {
-    data: coffeeShopByOwner,
-    refetch: coffeeShopByOwnerRefetch,
-    isLoading: coffeeShopByOwnerIsFetching,
-  } = useQuery(["CoffeeShopByOwner"], (): Promise<CoffeeShop> => getCoffeeShop(user.id), {
-    retry: false,
-  });
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route
-          path="/menu"
-          element={<Menu coffeeShop={coffeeShopByOwner} isFetching={coffeeShopByOwnerIsFetching} refetch={coffeeShopByOwnerRefetch} />}
-        />
-        <Route path="/qrcode" element={<QrCode />} />
-        {coffeeShops?.map((coffeeShop, key) => (
-          <Route
-            key={key}
-            path={`/${removeSpacesAndAccents(coffeeShop.name)}`}
-            element={<Menu coffeeShop={coffeeShop} isFetching={coffeeShopsIsFetching} refetch={coffeeShopsRefetch} isPublic />}
-          />
-        ))}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/qrcode" element={<QrCode />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
